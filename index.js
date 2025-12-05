916c3d2d28314d74c898eed04046c99dc6046afa
nikhilareddy28 <nikhilareddy9333@gmail.com>
2025-12-05 11:45:20 +0530

    refactor: replace http server with express framework

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-import http from 'http';
-const server = http.createServer((req,res) => {
-    console.log(req.method);
-    if(req.url == "/users"){
-        res.writeHead(200, {"Content-Type": "application/json"});
-        res.end(JSON.stringify({name: "thub"}))
-    } else {
-        res.writeHead(200, {"content-type": "text/html"});
-        res.end("Hello this is from backend");
-    }
-});
-server.listen(7007, () => {
-    console.log(`server running at port ${7007}`);
+// import http from 'http';
+// const server = http.createServer((req,res) => {
+//     console.log(req.method);
+//     if(req.url == "/users"){
+//         res.writeHead(200, {"Content-Type": "application/json"});
+//         res.end(JSON.stringify({name: "thub"}))
+//     } else {
+//         res.writeHead(200, {"content-type": "text/html"});
+//         res.end("Hello this is from backend");
+//     }
+// });
+// server.listen(7007, () => {
+//     console.log(`server running at port ${7007}`);
+// })
+
+
+
+import express from 'express';
+const app = express();
+app.get('/users', (req, res) => {
+    res.send("hello this is from backend");
 })
+app.get('/get-users', (req, res) => {
+    res.send("hello this is from backend");
+})
+app.listen(7007, () => {
+    console.log(`server running at port ${7007}`);
+});
